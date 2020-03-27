a5e6a55edc0522ed6c76590ce9984df915c53b27
Jiyuan Wang <wangjiyuan123@yeah.net>
2020-03-27 14:34:22 -0700

    add Applicable prgram

---
+++ Subjects/Applicable/A4.ExternalFunction/Spec_BigFuzz/filter1.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+public class filter1 {
+  static final boolean apply(int v_t2){
+  double v1=Math.log10(v_t2);
+  return v1 > 1;
+}
+
+}
---
+++ Subjects/Applicable/A4.ExternalFunction/Spec_BigFuzz/flatMap4.java
@@ -0,0 +1,7 @@
+
+
+public class flatMap4 {
+  static final String[] apply(String line){
+  return (line.split(","));
+}
+}
