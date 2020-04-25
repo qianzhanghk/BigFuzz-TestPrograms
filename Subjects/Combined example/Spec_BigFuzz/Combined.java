ca86f95d2f59ba0c209e8b54912d84910c2acae0
Jiyuan Wang <wangjiyuan123@yeah.net>
2020-04-24 17:29:37 -0700

    update example

--- Subjects/Combined example/Spec_BigFuzz/Combined.java
+++ Subjects/Combined example/Spec_BigFuzz/Combined.java
@@ -7,10 +7,10 @@ import java.util.ArrayList;
 
 public class Combined {
 public void Combined(String inputFile) throws IOException {
-ArrayList<String> results0 = CustomArray.read(inputFile);
+ArrayList<String> results0 = CustomArray.read(inputFile1);
 ArrayList<Tuple4> results1 = CombinedCustomArray.Map1(results0);
 ArrayList<Tuple2> results2 = CombinedCustomArray.Map2(results1);
-ArrayList<String> results3 = CustomArray.read(inputFile);
+ArrayList<String> results3 = CustomArray.read(inputFile2);
 ArrayList<map3> results4 = CombinedCustomArray.Map3(results3);
 ArrayList<map3> results5 = CombinedCustomArray.Filter1(results4);
 ArrayList<join2> results6 = CombinedCustomArray.Join1(results5, results2);
